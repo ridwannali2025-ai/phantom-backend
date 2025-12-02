9072b55fbde008c3463b26b14f5c51caffeb0512
Ridwan <Ridwan2001@icloud.com>
2025-12-01 20:58:24 -0600

    Initial Commit

---
+++ phantomAI/phantomAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  phantomAIApp.swift
+//  phantomAI
+//
+//  Created by Ridwan Ali on 12/1/25.
+//
+
+import SwiftUI
+
+@main
+struct phantomAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
